c2add8374e913ac9435c5414f260d96e4c2c7255
Maya Chisem <chisem.maya@gmail.com>
2020-08-11 12:47:10 -0700

    updated js functionality

---
+++ assets/js/categories.js
@@ -0,0 +1,16 @@
+//////////////////
+// GOOGLE MAPS/PLACES API KEY
+////////////////////// 
+
+var googleApiKey = "AIzaSyAhBVrpWoA9FUHSfRrpiB_4OOw2Crmlw-8"
+
+var googleMapsUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key="+googleApiKey
+
+
+$.ajax({
+    url: googleMapsUrl,
+    method: "GET"
+  }).then(function(response) {
+
+    console.log(response);        
+  })
