829535cdfa60e949d91586748b0653fdf808699b
Maya Chisem <chisem.maya@gmail.com>
2020-08-13 13:55:41 -0700

    updating city search function

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -18,16 +18,16 @@ getCity();
 
 var googleApiKey = "AIzaSyAhBVrpWoA9FUHSfRrpiB_4OOw2Crmlw-8";
 
-// var googleMapsUrl =
-//   "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=" +
-//   googleApiKey;
+var googleMapsUrl =
+  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=" +
+  googleApiKey;
 
-// $.ajax({
-//   url: googleMapsUrl,
-//   method: "GET",
-// }).then(function (response) {
-//   console.log(response);
-// });
+$.ajax({
+  url: googleMapsUrl,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+});
 
 
 //////////////////
