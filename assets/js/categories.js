d79cd70a46688548ee2b27d440ebda52628846e6
Maya Chisem <chisem.maya@gmail.com>
2020-08-13 13:30:24 -0700

    updatating css and js

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,9 +1,15 @@
 // 1. have to get the city input from index.html
-// var city = localStorage.getItem("city");
+var city = localStorage.getItem("city");
 // 2. convert the city input into latitude and longitude coordinates for google maps api
 // 3. convert that for google places OR maybe for another restaurant finding app
 // 4. have to filter that array of places into our four different categories- breakfast, lunch, dinner, and dessert
 // 5. when you click one of the filtered selections it takes you to that pages html
+
+function getCity() {
+  console.log(city);
+}
+
+getCity();
 
 //////////////////
 // GOOGLE MAPS/PLACES API KEY
@@ -23,36 +29,13 @@ var googleApiKey = "AIzaSyAhBVrpWoA9FUHSfRrpiB_4OOw2Crmlw-8";
 // });
 
 
-// longitude & lattitude locations
-var latitude = "12.91285";
-var longitude = "100.87808";
+//////////////////
+// UPDATED CITY SEARCH VALUE  
+//////////////////
+$("#search-btn").click(function(){
+  // var city = $("#search").val().trim();
+  localStorage.setItem("city", city)
 
-// Converts the city location into longitude and lattitude positions
-function getLatLngLocations(city) {}
-
-// Gets the list of restaurants upto 30 based on location
-function getRestaurants() {
-  var settings = {
-    url:
-      "https://tripadvisor1.p.rapidapi.com/restaurants/list-by-latlng?limit=30&currency=USD&distance=2&lunit=km&lang=en_US&latitude=" +
-      latitude +
-      "&longitude=" +
-      longitude,
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "569c506913msh97bb985d6e98504p1b3ad2jsnb268e32c19d8",
-    },
-  };
-
-  // Ajax call back function on query response
-  $.ajax(settings).done(function (response) {
-    for (var i = 0; i < response.data.length; i++) {
-      if (response.data[i] != undefined && response.data[i].name != undefined) {
-        console.log(response.data[i].name);
-      }
-    }
-  });
-}
-
-getLatLngLocations(city);
-getRestaurants();
+  location.replace("categories.html");
+ 
+});
