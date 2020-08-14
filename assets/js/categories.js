d6a3c663fb6b4a474aa18ef1adb64e068baae170
Maya Chisem <chisem.maya@gmail.com>
2020-08-13 19:12:35 -0700

    update

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -43,14 +43,19 @@ $.ajax({
     console.log(response);
   });
 
+  $("#breakfast").on("click", function(){
+    console.log("hello");
+  });
+  
 });
 
 // 3. have to filter that array of places into our four different categories- breakfast, lunch, dinner, and dessert
 // filter if the business is operational
   // if type of restaurant is a bar or food place it under dinner
   // if type of restaurant is food place under lunch
-// 4. when you click one of the filtered selections it takes you to that pages html
 
+
+// 4. when you click one of the filtered selections it takes you to that pages html
 // 5. make a search bar so the user can update their city input if they want
 //////////////////
 // UPDATED CITY SEARCH VALUE
