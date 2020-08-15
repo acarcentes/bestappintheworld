a20755eb045ca5a147abd140f75624d53aad14d5
Judith Nwagwu <Judith5nwagwu@gmail.com>
2020-08-15 15:28:25 -0700

    fixing css

--- assets/js/restaurants.js
+++ assets/js/restaurants.js
@@ -1,17 +1,11 @@
-// if business is temporarily closed don't display it
-/**
- * pulls information from the form and build the query URL
- * @returns {string} URL for NYT API based on form inputs
- */
-function buildQueryURL() {
-    // queryURL is the url we'll use to query the API
-    var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
-  
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "api-key": "AIzaSyAhBVrpWoA9FUHSfRrpiB_4OOw2Crmlw-8" };
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
+function buildQueryUrl() {
+  var geoCodingUrl =
+    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    city +
+    "&key=" +
+    googleApiKey;
+
+  var googleApiKey = "AIzaSyAhBVrpWoA9FUHSfRrpiB_4OOw2Crmlw-8";
+
+  googleApiKey.q = $("#search").val().trim();
+}
