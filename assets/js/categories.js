4689762b2017a56c808bdfcfc6dbae140e378112
Maya Chisem <chisem.maya@gmail.com>
2020-08-16 18:23:41 -0700

    updated styling and js

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -8,18 +8,8 @@ $(window).on("load", function () {
 });
 
 //////////////////
-// GRABBING THE CITY INPUT FROM INDEX.HTML FILE
-//////////////////
-var lat = localStorage.getItem("lat", lat);
-var lng = localStorage.getItem("lng", lng);
-
-function getCityCor() {
-  console.log(lat);
-  console.log(lng);
-}
-getCityCor();
-
 // Restaurant object with properties
+//////////////////
 var restaurantObj = [
   {
     name: "",
@@ -31,8 +21,10 @@ var restaurantObj = [
   },
 ];
 
+//////////////////
 //  Restaurant arrays
-var breakFastRestaurants = new Array();
+//////////////////
+var breakfastRestaurants = new Array();
 var lunchRestaurants = new Array();
 var dinnerRestaurants = new Array();
 var dessertRestaurants = new Array();
@@ -42,7 +34,7 @@ var dessertRestaurants = new Array();
 //////////////////////
 var googleApiKey = "AIzaSyAhBVrpWoA9FUHSfRrpiB_4OOw2Crmlw-8";
 
-function googlePlaces(foodType) {
+function googlePlaces() {
   var googlePlacesUrl =
     "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
     lat +
@@ -57,77 +49,239 @@ function googlePlaces(foodType) {
   }).then(function (response) {
     console.log(response);
 
-    // var results = response.results[0];
-    // console.log(results);
+    for (var i = 0; i < response.results.length; i++) {
+      var restaurant = Object.create(restaurantObj);
 
-    // var businessStatus = response.results[0].business_status;
-    // console.log(businessStatus)
+      if (response.results[i].name != undefined) {
+        var name = response.results[i].name;
+        restaurant.name = name;
+        console.log(name);
 
-    // for (var i = 0; i < results.length; i++) {
-    //   var restaurantLength = results[i];
-    //   console.log(restaurantLength)
-    // }
+        var div = $("<div>");
+        div.attr("data-name", name);
+        div.text(name);
+        $("#restaurants").append(div);
 
-    // business_status: "CLOSED_TEMPORARILY"
+      } else {
+        restaurant.name = " ";
+      }
 
-    if (foodType == "breakfast") {
+      if (response.results[i].vicinity != undefined) {
+        var address = response.results[i].vicinity;
+        restaurant.address = address;
+        console.log(address);
+        
+        localStorage.setItem("address", address);
+      } else {
+        restaurant.address = " ";
+      }
 
-      var breakfastArray = ["restaurant", "food", "bakery", "cafe"];
-    }
+      if (response.results[i].rating != undefined) {
+        var rating = response.results[i].rating;
+        restaurant.rating = rating;
+        console.log(rating);
+      } else {
+        restaurant.rating = " ";
+      }
 
-    if (foodType == "lunch") {
+      if (response.results[i].price_level != undefined) {
+        var priceLevel = response.results[i].price_level;
+        restaurant.priceLevel = priceLevel;
+        console.log(priceLevel);
+      } else {
+        restaurant.priceLevel = " ";
+      }
 
-      var lunchArray = ["restaurant", "food", "establishment", "store"];
+      if (response.results[i].opening_hours != undefined) {
+        var openNow = response.results[i].opening_hours.open_now;
+        restaurant.openNow = openNow;
+        console.log(openNow);
+      } else {
+        restaurant.openNow = "Closed";
+      }
 
-    }
+      if (
+        response.results[i].photos.length > 0 &&
+        response.results[i].photos[0].html_attributions.length > 0
+      ) {
+        var locImage = response.results[i].photos[0].html_attributions[0];
+        restaurant.locationImage = locImage;
+        console.log(restaurant.locationImage);
+      } else {
+        restaurant.locationImage = " ";
+      }
 
-    if (foodType == "dinner") {
+      for (var j = 0; j < response.results[i].types.length; j++) {
+        if (
+          response.results[i].types[j] == "bar" ||
+          response.results[i].types[j] == "night_club" || 
+          response.results[i].types[j] == "point_of_interest"
 
-      var dinnerArray = ["restaurant", "nightclub", "bar", "store"];
-    }
+        ) {
+          dinnerRestaurants.push(restaurant);
 
-    if (foodType == "dessert") {
+          break;
 
-      var dessertArray = ["restaurant", "bakery", "cafe"];
+        } else if (
+          response.results[i].types[j] == "restaurant" ||
+          response.results[i].types[j] == "bar" ||
+          response.results[i].types[j] == "food" ||
+          response.results[i].types[j] == "cafe" ||
+          response.results[i].types[j] == "bakery"
 
+        ) {
+          lunchRestaurants.push(restaurant);
+
+          break;
+
+        } else if (
+          response.results[i].types[j] == "restaurant" ||
+          response.results[i].types[j] == "food" ||
+          response.results[i].types[j] == "cafe" ||
+          response.results[i].types[j] == "store" ||
+          response.results[i].types[j] == "bakery"
+
+        ) {
+          breakfastRestaurants.push(restaurant);
+          lunchRestaurants.push(restaurant);
+          dessertRestaurants.push(restaurant);
+
+          break;
+        }
+      }
     }
   });
 }
-
-// if foodTypes(from data array) is equal to the breakfast array elements make a for loop if type = breakfastArray[i] then display
 
 $(".cell").on("click", function () {
   var id = this.id.toString();
   googlePlaces(id);
   console.log(id);
 
-  if(id === "breakfast") {
+  if(id === "Breakfast") {
+
+    var id = localStorage.setItem("id", id);
+
+  
+    for (var i = 0; i < breakfastRestaurants.length; i++) {
+      // console.log(breakfastRestaurants[i]);
+
+      if (breakfastRestaurants.length == 0) {
+        alert("There are no breakfast restaurants in " + city);
+        return;
+      }
+      // Create HTML components from the breakfast restaurants object array
+
+      var name = $("<h3>").text(breakfastRestaurants.name)
+
+          // var address = breakfastRestaurants.address
+      // var rating = breakfastRestaurants.rating
+
+      // var price = breakfastRestaurants.priceLevel
+      // var open = breakfastRestaurants.openNow
+      // var image = breakfastRestaurants.locationImage
+
+    }
+
     // location.replace("restaurants.html");
+
   }
 
-  if(id === "lunch") {
-    // location.replace("restaurants.html");
+  if(id === "Lunch") {
+
+    var id = localStorage.setItem("id", id);
+
+    for (var i = 0; i < lunchRestaurants.length; i++) {
+      console.log(lunchRestaurants[i]);
+
+      if (lunchRestaurants.length == 0) {
+        alert("There are no lunch restaurants in " + city);
+        return;
+      }
+
+    }
+
   }
 
-  if(id === "dinner") {
-    // location.replace("restaurants.html");
+  if(id === "Dinner") {
+
+    var id = localStorage.setItem("id", id);
+
+    for (var i = 0; i < dinnerRestaurants.length; i++) {
+      console.log(dinnerRestaurants[i]);
+
+      if (dinnerRestaurants.length == 0) {
+        alert(
+          "There are no dinner restaurants in " + city
+        );
+        return;
+      }
+
+    }
   }
 
-  if(id === "dessert") {
-    // location.replace("restaurants.html");
+  if(id === "Dessert") {
+
+    var id = localStorage.setItem("id", id);
+
+    for (var i = 0; i < dessertRestaurants.length; i++) {
+      console.log(dessertRestaurants[i]);
+
+      if (dessertRestaurants.length == 0) {
+        alert(
+          "There are no dessert restaurants in " + city
+        );
+        return;
+      }
+  
+    }
   }
+
 });
 
-// 3. have to filter that array of places into our four different categories- breakfast, lunch, dinner, and dessert
-// filter if the business is operational
 // 4. when you click one of the filtered selections it takes you to the restaurant pages html
-
 // 5. make a search bar so the user can update their city input if they want
 //////////////////
 // UPDATED CITY SEARCH VALUE
 //////////////////
 $("#search").click(function () {
   var city = $("#city").val().trim();
-  localStorage.setItem("city", city);
-  location.replace("categories.html");
+
+  var geoCodingUrl =
+    "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    city +
+    "&key=" +
+    googleApiKey;
+
+  $.ajax({
+    url: geoCodingUrl,
+    method: "GET",
+  }).then(function (response) {
+
+    var lat = response.results[0].geometry.location.lat;
+    console.log(lat);
+
+    var lng = response.results[0].geometry.location.lng;
+    console.log(lng);
+
+    localStorage.setItem("city", city);
+    localStorage.setItem("lat", lat);
+    localStorage.setItem("lng", lng);
+    location.replace("categories.html");
+  });
 });
+
+
+//////////////////
+// GRABBING THE CITY AND LAT/LNG INPUT FROM LOCAL STORAGE
+//////////////////
+var city = localStorage.getItem("city", city);
+var lat = localStorage.getItem("lat", lat);
+var lng = localStorage.getItem("lng", lng);
+
+function getCityCor() {
+  console.log(city);
+  console.log(lat);
+  console.log(lng);
+}
+getCityCor();
--- assets/js/restaurants.js
+++ assets/js/restaurants.js
@@ -7,17 +7,23 @@ $(window).on("load", function () {
   $(loader).fadeOut("slow");
 });
 
-// function buildQueryUrl() {
-//   var geoCodingUrl =
-//     "https://maps.googleapis.com/maps/api/geocode/json?address=" +
-//     city +
-//     "&key=" +
-//     googleApiKey;
+//////////////////
+// GET FOOD CATEGORY AND CITY SEARCH VALUE
+//////////////////
+var id = localStorage.getItem("id", id);
+var city = localStorage.getItem("city", city);
 
-//   var googleApiKey = "AIzaSyAhBVrpWoA9FUHSfRrpiB_4OOw2Crmlw-8";
+function foodAndCity() {
+  var foodCategory = $("#foodCategory");
+  foodCategory.text(id).css({"color": "cadetblue", "text-transform": "uppercase"});
 
-//   googleApiKey.q = $("#search").val().trim();
-// }
+  var location = $("#location");
+  location.text(city).css({"color": "cadetblue", "text-transform": "uppercase"});
+
+}
+
+foodAndCity();
+
 
 //////////////////
 // UPDATED CITY SEARCH VALUE
